4f3a889d1d7457929c63ee48e095f9bb5dcbcdb1
Randy DuCharme <radio.ad5gb@gmail.com>
2015-02-23 16:16:44 -0600

    Update mk
    Signed-off-by: Randy DuCharme <radio.ad5gb@gmail.com>

--- broken.mk
+++ broken.mk
@@ -5,6 +5,9 @@ TARGET_SCREEN_WIDTH  := 1200
 # Release name
 PRODUCT_RELEASE_NAME := deb
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
 # Inherit some common stuff.
 $(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
 
@@ -21,11 +24,15 @@ PRODUCT_PACKAGES += \
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := deb
 PRODUCT_NAME := broken_deb
-PRODUCT_BRAND := Google
-PRODUCT_MODEL := Nexus 7
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Nexus 7 2013
 PRODUCT_MANUFACTURER := Asus
-
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/asus/deb/device.mk)
+$(call inherit-product-if-exists, vendor/asus/deb/deb-vendor.mk)
+#$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT=google/razor/deb:4.4.3/KTU84L/1148727:user/release-keys PRIVATE_BUILD_DESC="razor-user 4.4.3 KTU84L 1148727 release-keys"
