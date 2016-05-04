616bb5f1b495dc4029878087e3788c34eeaf9a49
CrackerTeg <crackerteg@gmail.com>
2016-05-04 18:42:34 -0500

    SnapdragonCamera is now called in common
    Signed-off-by: CrackerTeg <crackerteg@gmail.com>

--- device.mk
+++ device.mk
@@ -28,8 +28,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 PRODUCT_PACKAGES += \
-	camera.deb \
-	SnapdragonCamera
+	camera.deb
 
 #NFC
 PRODUCT_PACKAGES += \
